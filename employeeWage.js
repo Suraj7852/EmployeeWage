0074c9aa5cbc6e1add63d128584b7d55151f977b
suraj7852 <surajpj7852@gmail.com>
2019-12-26 17:01:10 +0530

    Added functionality for total working hour in a month

--- employeeWage.js
+++ employeeWage.js
@@ -1,13 +1,18 @@
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
+let MAX_HRS_IN_MONTH = 10;
 let EMP_RATE_PER_HOUR = 20;
 let NUM_OF_WORKING_DAYS = 2;
 
 let empHrs = 0;
-let empWage = 0;
+let totalEmpHrs = 0;
 let totalWage = 0;
-for(let day=0; day<NUM_OF_WORKING_DAYS; day++) {
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+	totalWorkingDays <= NUM_OF_WORKING_DAYS) {
 	let empCheck = Math.floor(Math.random()*3);
+	totalWorkingDays++;totalEmpHrs
 	switch(empCheck) {
 		case IS_PART_TIME:
 			empHrs = 4;
@@ -18,7 +23,7 @@ for(let day=0; day<NUM_OF_WORKING_DAYS; day++) {
 		default:
 			empHrs = 0;
 	}
-empWage = empHrs*EMP_RATE_PER_HOUR;
-totalWage += empWage;
+	totalEmpHrs += empHrs;
 }
-console.log(totalWage)
+totalWage = totalEmpHrs*EMP_RATE_PER_HOUR;
+console.log(totalWage);
