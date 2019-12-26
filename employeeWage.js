7065f9738e035299b537cdb7fb6c29766ffd7be0
suraj7852 <surajpj7852@gmail.com>
2019-12-26 17:17:15 +0530

    Refactored code and inplemented function

--- employeeWage.js
+++ employeeWage.js
@@ -9,20 +9,25 @@ let totalEmpHrs = 0;
 let totalWage = 0;
 let totalWorkingDays = 0;
 
+function getWorkingHours(empCheck) {
+	switch(empCheck) {
+                case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                default:
+                        empHrs = 0;
+        }
+	return empHrs;
+
+}
 while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 	totalWorkingDays <= NUM_OF_WORKING_DAYS) {
 	let empCheck = Math.floor(Math.random()*3);
-	totalWorkingDays++;totalEmpHrs
-	switch(empCheck) {
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
-	}
+	totalWorkingDays++;
+	empHrs = getWorkingHours(empCheck);
 	totalEmpHrs += empHrs;
 }
 totalWage = totalEmpHrs*EMP_RATE_PER_HOUR;
